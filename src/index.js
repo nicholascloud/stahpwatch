6a477dd5b9ced58243f5befecacd808194f23198
nicholascloud <me@nicholascloud.com>
2017-07-18 11:36:58 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+'use strict';
+const StopWatch = require('timer-stopwatch');
+const moment = require('moment');
+require('moment-duration-format');
+const argv = require('minimist')(process.argv.slice(2));
+
+const usage = function () {
+    console.info('usage: node src/index.js [--duration=#]');
+};
+
+if (argv.help) {
+    usage();
+    process.exit(0);
+}
+
+let timer;
+if (argv.duration) {
+    const milliseconds = parseInt(argv.duration, 10) * 1000;
+    timer = new StopWatch(milliseconds);
+} else {
+    timer = new StopWatch();
+}
+
+let lastTime;
+
+timer.onTime((time) => {
+    if (lastTime) {
+        const len = lastTime.length;
+        const bs = '\b'.repeat(len);
+        process.stdout.write(bs);
+    }
+    lastTime = moment.duration(time.ms, 'millisecond')
+        .format('hh:mm:ss', { trim: false });
+    process.stdout.write(lastTime);
+});
+
+timer.start();
